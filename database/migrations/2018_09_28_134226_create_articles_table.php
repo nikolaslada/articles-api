038d13042a8685ce5e1d7ade9b461c887a4e8750
Nikolas Lada <nikolas.lada@gmail.com>
2018-09-28 23:26:38 +0200

    space correction

--- database/migrations/2018_09_28_134226_create_articles_table.php
+++ database/migrations/2018_09_28_134226_create_articles_table.php
@@ -9,17 +9,17 @@ class CreateArticlesTable extends Migration
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
-              $table->mediumIncrements('id')->unsigned();
-              $table->unsignedMediumInteger('authors_id');
-              $table->foreign('authors_id')->references('id')->on('authors');
-              $table->string('uid', 100)->unique();
-              $table->string('title', 255);
-              $table->enum('status', [
-                  'hidden',
-                  'published',
-              ]);
-              $table->text('content');
-              $table->timestamps();
+            $table->mediumIncrements('id')->unsigned();
+            $table->unsignedMediumInteger('authors_id');
+            $table->foreign('authors_id')->references('id')->on('authors');
+            $table->string('uid', 100)->unique();
+            $table->string('title', 255);
+            $table->enum('status', [
+                'hidden',
+                'published',
+            ]);
+            $table->text('content');
+            $table->timestamps();
         });
     }
 
